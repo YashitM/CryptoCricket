81e5683871929908b38088181c6de2d5988997d5
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-07-20 19:38:44 +0530

    Collected Static Files

--- cricket/settings.py
+++ cricket/settings.py
@@ -121,6 +121,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
