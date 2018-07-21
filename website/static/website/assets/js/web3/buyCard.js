c66d3c543db8ec416eceec179cbd11d28bf614a0
Piyush Gupta <piyush16066@iiitd.ac.in>
2018-07-21 22:36:42 +0530

    Added wallet address check

--- website/static/website/assets/js/web3/buyCard.js
+++ website/static/website/assets/js/web3/buyCard.js
@@ -1,28 +1,48 @@
 // Trigger this function when user wants to buy a card
 // @param tokenID: integer: unique ID of the card
 // @param price: integer: selling price of the card to be bought
-function buyCard(tokenID, price)
+// @param walletAddress: string: ethereum wallet address of user
+function buyCard(tokenID, price, walletAddress)
 {
-    console.log("LDJkhfksjhd");
-	var myEvent = cryptoCricketInstance.LogSnatch({},{fromBlock: web3.eth.getBlockNumber(function(error, result){ console.log(result)}), toBlock: 'latest'});
-
-	cryptoCricketInstance.purchase(tokenID, {value: web3.toWei(price, "ether")}, function(error,result)
+	var currentAcc = "";
+	var storedAcc = walletAddress.toLowerCase();
+	web3.eth.getCoinbase(function(err,account)
 	{
-		if(error)
-		console.error(error);
+		if(err === null)
+		{
+			currentAcc = account;
+			var myEvent = cryptoCricketInstance.LogSnatch({},{fromBlock: web3.eth.getBlockNumber(function(error, result){ console.log(result)}), toBlock: 'latest'});
+
+			if(currentAcc !== storedAcc)
+			{
+				alert("Please Use Wallet: " + walletAddress + " to make the transaction");
+				return;
+			}
+
+			cryptoCricketInstance.purchase(tokenID, {value: web3.toWei(price, "ether")}, function(error,result)
+			{
+				if(error)
+					console.error(error);
+				else
+				{
+					myEvent.watch(function(error, result)
+					{
+						console.log("Bought Card Successfully"); 
+						console.log(JSON.stringify(result.args.tokenId));
+						console.log(JSON.stringify(result.args.tokenName));
+						console.log(JSON.stringify(result.args.oldOwner));
+						console.log(JSON.stringify(result.args.newOwner));
+						console.log(JSON.stringify(result.args.oldPrice));
+						console.log(JSON.stringify(result.args.newPrice));
+					});
+				}
+			})
+			myEvent.stopWatching();
+		}
 		else
 		{
-			myEvent.watch(function(error, result)
-			{
-				console.log("Bought Card Successfully");
-				console.log(JSON.stringify(result.args.tokenId));
-				console.log(JSON.stringify(result.args.tokenName));
-				console.log(JSON.stringify(result.args.oldOwner));
-				console.log(JSON.stringify(result.args.newOwner));
-				console.log(JSON.stringify(result.args.oldPrice));
-				console.log(JSON.stringify(result.args.newPrice));
-			});
+			console.log(err);
 		}
-	})
-	myEvent.stopWatching();
+	});
+	return;
 }
